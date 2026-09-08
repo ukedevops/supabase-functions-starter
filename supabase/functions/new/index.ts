2036730275cc0247a447ac0d534eed8510b1a328
Ukendiran <ukendiran@gmail.com>
2026-09-08 10:28:22 +0530

    new

---
+++ supabase/functions/new/index.ts
@@ -0,0 +1,10 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+
+serve(async () => {
+  return new Response(JSON.stringify({
+    success: true,
+    message: "Hello from Supabase Edge Function"
+  }), {
+    headers: { "Content-Type": "application/json" }
+  });
+});
