792f86f7da39f15ea85109749f10d1ef3167379a
Ukendiran <ukendiran@gmail.com>
2026-09-08 19:02:12 +0530

    changes

--- supabase/functions/hello/index.ts
+++ supabase/functions/hello/index.ts
@@ -1,5 +1,6 @@
 import { serve } from "https://deno.land/std/http/server.ts";
 
+
 serve(async () => {
   return new Response(JSON.stringify({
     success: true,
--- supabase/functions/new/index.ts
+++ supabase/functions/new/index.ts
@@ -1,7 +1,5 @@
 import { serve } from "https://deno.land/std/http/server.ts";
 
-
-
 serve(async () => {
   return new Response(JSON.stringify({
     success: true,
